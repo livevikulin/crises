c1d2a9f1dfc24d73d63be856cc72307c7fa820f6
Vikulin Vitaly <livevikulin@gmail.com>
2020-01-20 20:23:29 +0700

    add files in repository

---
+++ js/script.js
@@ -0,0 +1,66 @@
+$( document ).ready(function() {
+    $('body').on('click', '.more__toggle', function(){
+        $(this).parents('.more').find('.more__content').slideToggle();
+        $(this).parents('.more').toggleClass('open');
+        if($(this).parents('.more').hasClass('open')){
+            $(this).text('Скрыть список');
+        } else {
+            $(this).text('Раскрыть список');
+        }
+    })
+
+    $(document).one('mouseleave', function(e){
+        if (e.clientY < 0) {
+            $('.modal').addClass('open');
+        }     
+    });
+
+    function closeModal(){
+        $('.modal__close').parents('.modal').removeClass('open');
+    }
+
+    $('body').on('click', '.modal__close', function(){
+        closeModal();
+    })
+
+    jQuery(function($){
+        $(document).mouseup(function (e){
+            var div = $(".modal__wrap");
+            if (!div.is(e.target)
+                && div.has(e.target).length === 0) {
+                closeModal();
+            }
+        });
+    });
+
+    var $page = $('html, body');
+    $('a.anchor-link[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
+
+
+    
+    if (document.body.clientWidth > 1024) {
+	
+		function parallax (event) {
+			
+			this.querySelectorAll('.parallax').forEach(planet => {
+			
+				let speed = planet.getAttribute('data-speed'),
+					mouseMove = `translate(${event.clientX * speed / 750}px, ${event.clientY * speed / 750}px)`;
+				
+				planet.style.transform = mouseMove;
+				
+			});
+		}
+		
+		document.addEventListener('mousemove', parallax);
+
+	};
+    
+
+});
